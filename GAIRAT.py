9a8dfacb8baf34d7230894d0221f5aae0c880034
Bongsoo Yi <austinyi@naver.com>
2022-05-22 15:39:45 +0900

    d

--- GAIRAT.py
+++ GAIRAT.py
@@ -267,7 +267,7 @@ for epoch in range(start_epoch, args.epochs):
         )
          
     logger_test.append([epoch + 1, test_nat_acc, test_pgd20_acc])
-    
+ '''   
     # Save the best checkpoint
     if test_pgd20_acc > best_acc:
         best_acc = test_pgd20_acc
@@ -287,5 +287,5 @@ for epoch in range(start_epoch, args.epochs):
                 'test_pgd20_acc': test_pgd20_acc,
                 'optimizer' : optimizer.state_dict(),
             })
-    
+ '''
 logger_test.close()
