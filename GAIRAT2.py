db59814e6b4bcf1164186e01270981401c0d2e8a
Bongsoo Yi <austinyi@naver.com>
2022-06-02 17:51:45 +0900

    d

--- GAIRAT2.py
+++ GAIRAT2.py
@@ -167,8 +167,7 @@ def trainClassifier(args, model, train_loader, test_loader, use_cuda=True):
         Lambda = adjust_Lambda(epoch + 1)
         #num_data = 0
         #train_robust_loss = 0
-        print(train_loader)
-        for idx, x, target in tqdm(train_loader):
+        for idx, (x, target) in enumerate(train_loader):
             x, target = to_var(x), to_var(target)
 
             x_adv, Kappa = attack.GA_PGD(model, data, target, args.epsilon, args.step_size, args.num_steps,
